b6608d5236d9833e339f7cfec59a4bb7d948a878
Sylvia Mullins <49255017+Helasrebellion@users.noreply.github.com>
2023-07-20 22:30:40 -0400

    trying to get it to build on aws

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App1 from './App1';
 import './index.css';
+import axios from 'axios';
+
 
 // Render the App1 component inside the root element
 ReactDOM.render(<App1 />, document.getElementById('root'));
